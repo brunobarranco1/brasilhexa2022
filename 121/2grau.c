513a2dab495903fdcebe67480010adbc2155c17f
brunobarranco1 <116804751+brunobarranco1@users.noreply.github.com>
2023-01-15 19:54:45 -0300

    Create 2grau.c

---
+++ 121/2grau.c
@@ -0,0 +1,48 @@
+/*
+ * 2grau.c
+ * data: 12/01
+ * Bruno Barranco 12121EEL025
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int raizes(float a, float b, float c, float *x1, float *x2);
+
+int main(int argc, char** argv){
+	
+    float a,b,c,r,x1,x2;
+
+    printf("manda o a,b,c:\n");
+    scanf("%f %f %f%*c", &a, &b, &c);
+
+    r = raizes(a, b,c, &x1,&x2);
+
+    printf("%lg\n", r);
+
+    if(r == 2){
+        printf("%f %f\n", x1, x2);
+    }else if(r ==1){
+        printf("%f\n", x1);
+    }
+
+	return 0;
+}
+
+int raizes(float a, float b, float c, float *x1, float *x2){
+    int delta;
+
+    delta = (b*b)-(4*a*c);
+
+    if(delta > 0){
+        *x1 = ((-1)*b+sqrt(delta))/(2*a);
+        printf("%lg %lg\n", sqrt(delta), a);
+        *x2 = ((-1)*b-sqrt(delta))/(2*a);
+        return 2;
+    }else if(delta == 0){
+        *x1 = ((-1)*b+sqrt(delta))/(2*a);
+        return 1;
+    }
+    return 0;
+}
